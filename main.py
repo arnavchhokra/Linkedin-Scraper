c1cc1639af3b0ffa5b3517af3b50d5fcfd848b22
Arnav Chhokra <arnavchhokra@gmail.com>
2024-02-22 16:50:47 +0530

    login working

---
+++ main.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+
+
+driver.get("https://www.linkedin.com/login")
+
+def login(driver):
+    email = driver.find_element(by=By.ID,value="username")
+    email.send_keys("work.arnavchhokra@gmail.com")
+    pwd = driver.find_element(by=By.ID,value="password")
+    pwd.send_keys("#1Arnavchhokra")
+    submit= driver.find_element(By.CSS_SELECTOR, "button.btn__primary--large")
+    submit.click()
+    time.sleep(3)
+    getProfileURl()
+
+
+def getProfileURl():
+    time.sleep(30)
+    finder = driver.find_element(By.CSS_SELECTOR,"search-global-typeahead__input")
+    finder.send_keys("Ycombinator")
+    search = driver.find_element(By.CSS_SELECTOR,"search-global-typeahead__search-icon-container")
+    search.click()
+
+
+
+
+login(driver)
